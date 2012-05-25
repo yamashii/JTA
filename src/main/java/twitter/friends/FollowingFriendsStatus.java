e2684ed611e677ba31dc249a3cf4a3d4f6c9f2e9
Masato Yamashita <yamashita@mk.ces.kyutech.ac.jp>
2012-05-25 14:36:13 +0900

    フォロワーの取得、フォローしている人の取得を行うクラスを作成しました。

---
+++ src/main/java/twitter/friends/FollowingFriendsStatus.java
@@ -0,0 +1,32 @@
+package twitter.friends;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+
+/**
+ * @author yamashita
+ * @version $Revision$, 2012/05/25
+ */
+public class FollowingFriendsStatus {
+
+  private Twitter twitter;
+
+  /**
+   * Initialize the generated object of {@link FollowingFriendsStatus}.
+   * 
+   * @param twitter
+   */
+  public FollowingFriendsStatus(Twitter twitter) {
+    this.twitter = twitter;
+  }
+
+  /**
+   * 
+   */
+  public void searchFollowingUser() {
+    
+  }
+
+  
+}
--- src/main/java/twitter/index/TweetMain.java
+++ src/main/java/twitter/index/TweetMain.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import twitter.access.TwitterAccessor;
+import twitter.friends.FollowedFriendsStatus;
+import twitter.friends.FollowingFriendsStatus;
 import twitter.tl.friends.FriendsTimeLine;
 import twitter.tl.own.MyTweetTimeLineDisplay;
 import twitter.tweet.TweetInput;
@@ -40,6 +42,8 @@ public class TweetMain {
       System.out.println("2：自分のつぶやき履歴を見る"); //$NON-NLS-1$
       System.out.println("3：他の人のつぶやきを見る"); //$NON-NLS-1$
       System.out.println("4:ユーザの名前全てを見る"); //$NON-NLS-1$
+      System.out.println("5:フォローしている人取得"); //$NON-NLS-1$
+      System.out.println("6:フォロワー取得"); //$NON-NLS-1$
       System.out.println("9:終了"); //$NON-NLS-1$
       String actNum;
       try {
@@ -59,6 +63,12 @@ public class TweetMain {
       } else if (actNum.equals("4")) { //$NON-NLS-1$
         UsersListSearch listSearch = new UsersListSearch(twitter);
         listSearch.searchUsersList();
+      } else if (actNum.equals("5")) { //$NON-NLS-1$
+        FollowingFriendsStatus status = new FollowingFriendsStatus(twitter);
+        status.searchFollowingUser();
+      } else if (actNum.equals("6")) { //$NON-NLS-1$
+        FollowedFriendsStatus status = new FollowedFriendsStatus(twitter);
+        status.searchFollowedUser();
       } else if (actNum.equals("9")) { //$NON-NLS-1$
         condition = false;
       } else {
